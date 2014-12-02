4688d71da2a78d387016f89c4315005797a70e2c
KqStone <kqstone@163.com>
2014-12-02 11:24:58 +0800

    switch debug settings display

--- src/com/kqstone/immersedstatusbar/SettingsActivity.java
+++ src/com/kqstone/immersedstatusbar/SettingsActivity.java
@@ -6,7 +6,9 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import de.robv.android.xposed.XposedHelpers;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -17,12 +19,15 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.widget.Toast;
 
 public class SettingsActivity extends PreferenceActivity implements OnPreferenceClickListener, OnPreferenceChangeListener{
 	private final static String KEY_PREF_ABOUT = "about";
 	private final static String KEY_PREF_PROFILE = "profile";
+	private static final String KEY_SWITCH_DEBUG = "switch_debug";
 	
 	private Context mContext;
 	private Preference mPrefAbout;
@@ -30,8 +35,12 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 	private CheckBoxPreference mPrefForceTint;
 	private CheckBoxPreference mPreTintNotification;
 	private CheckBoxPreference mPreQuickAnimContent;
+	private PreferenceCategory mDebugprefCategory;
+	private Preference mSwitchDebug;
 	private CheckBoxPreference mPreExptInform;
 	private CheckBoxPreference mPreExptInformToFile;
+	
+	private boolean mDebugMode = false;
 	
 	private Handler handler = new Handler() {
 		@Override
@@ -61,6 +70,43 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 		
 	}
 	
+	class Dialog {
+		public static final int ALERT_DEBUG = 0;
+		private AlertDialog.Builder mBuilder;
+		private Context mContext;
+		private AlertDialog mDialog;
+		
+		public Dialog(Context context, int what) {
+			mContext = context;
+			mBuilder = new AlertDialog.Builder(context);
+			switch (what) {
+			case 0:
+				mBuilder.setTitle(R.string.title_waring_debug);
+				mBuilder.setMessage(R.string.message_waring_debug);
+				mBuilder.setNegativeButton(R.string.button_cancel, new DialogInterface.OnClickListener(){
+
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) {
+						if (mDialog != null)
+							mDialog.dismiss();
+					}});
+				mBuilder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener(){
+
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) {
+						onDebugSwitchChanged(false);
+					}});
+				break;
+				default: break;
+			}
+			mDialog = mBuilder.create();
+		}
+		
+		public void show() {
+			mDialog.show();
+		}
+	}
+	
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -81,12 +127,23 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 		mPreQuickAnimContent = (CheckBoxPreference) findPreference(Constant.KEY_PREF_QUICKANIM_CONTENT);
 		mPreQuickAnimContent.setChecked(Settings.System.getInt(getContentResolver(), Constant.KEY_PREF_QUICKANIM_CONTENT, 0) ==1 ? true:false);
 		mPreQuickAnimContent.setOnPreferenceChangeListener(this);
+		mDebugprefCategory = (PreferenceCategory)findPreference("debug");
+		mSwitchDebug = findPreference(KEY_SWITCH_DEBUG);
+		mSwitchDebug.setOnPreferenceClickListener(this);
 		mPreExptInform = (CheckBoxPreference) findPreference(Constant.KEY_PREF_EXPORT_INFORM);
-		mPreExptInform.setChecked(Settings.System.getInt(getContentResolver(), Constant.KEY_PREF_EXPORT_INFORM, 0) ==1 ? true:false);
+		boolean expInform = Settings.System.getInt(getContentResolver(), Constant.KEY_PREF_EXPORT_INFORM, 0) ==1 ? true:false;
+		mPreExptInform.setChecked(expInform);
 		mPreExptInform.setOnPreferenceChangeListener(this);
 		mPreExptInformToFile = (CheckBoxPreference) findPreference(Constant.KEY_PREF_EXPORT_INFORM_TOFILE);
-		mPreExptInformToFile.setChecked(Settings.System.getInt(getContentResolver(), Constant.KEY_PREF_EXPORT_INFORM_TOFILE, 0) ==1 ? true:false);
+		boolean expInformToFile = Settings.System.getInt(getContentResolver(), Constant.KEY_PREF_EXPORT_INFORM_TOFILE, 0) ==1 ? true:false;
+		mPreExptInformToFile.setChecked(expInformToFile);
 		mPreExptInformToFile.setOnPreferenceChangeListener(this);
+		if (expInform || expInformToFile) {
+			new Dialog(mContext, Dialog.ALERT_DEBUG).show();
+			mDebugMode = true;
+		}
+		onDebugSwitchChanged(mDebugMode);
+		
 	}
 
 	@Override
@@ -104,6 +161,8 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 							R.string.begin_download_profiles),
 					Toast.LENGTH_SHORT).show();
 			new Thread(new DownLoadThread()).start();
+		} else if (key.equals(KEY_SWITCH_DEBUG)) {
+			onDebugSwitchChanged(!mDebugMode);
 		}
 		return false;
 	}
@@ -132,6 +191,23 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 		}
 		
 		return false;
+	}
+	
+	public void onDebugSwitchChanged(boolean switched) {
+		mDebugMode = switched;
+		if(!switched) {
+			mSwitchDebug.setTitle(R.string.show_debug_settings);
+			mDebugprefCategory.removePreference(mPreExptInform);
+			mDebugprefCategory.removePreference(mPreExptInformToFile);
+			Settings.System.putInt(mContext.getContentResolver(), Constant.KEY_PREF_EXPORT_INFORM, 0);
+			Settings.System.putInt(mContext.getContentResolver(), Constant.KEY_PREF_EXPORT_INFORM_TOFILE, 0);
+			mPreExptInform.setChecked(false);
+			mPreExptInformToFile.setChecked(false);
+		} else {
+			mSwitchDebug.setTitle(R.string.hide_debug_settings);
+			mDebugprefCategory.addPreference(mPreExptInform);
+			mDebugprefCategory.addPreference(mPreExptInformToFile);
+		}
 	}
 
 }
