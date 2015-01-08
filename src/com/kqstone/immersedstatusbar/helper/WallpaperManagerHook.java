fe35aaae46cf077defa09da2fae03e95d99a703b
KqStone <kqstone@163.com>
2015-01-08 21:58:44 +0800

    wallpaperManagerHook

---
+++ src/com/kqstone/immersedstatusbar/helper/WallpaperManagerHook.java
@@ -0,0 +1,18 @@
+package com.kqstone.immersedstatusbar.helper;
+
+import com.kqstone.immersedstatusbar.Const;
+import com.kqstone.immersedstatusbar.Utils;
+
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.Intent;
+
+public class WallpaperManagerHook {
+
+	public static void hookAfterSetWallpaper(Object wallpaperManager) {
+		Intent intent = new Intent(Const.INTENT_SET_WALLPAPER);
+		Context context = (Context) ReflectionHelper.getObjectField(wallpaperManager, "mContext");
+		context.sendBroadcast(intent);
+		Utils.log("Send change wallpaper broadcast...");
+	}
+}
